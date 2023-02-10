8db9749426acb9c7ae403cdee5aeb101976b0d85
Kon-Geo <kongeo2008@gmail.com>
2023-02-10 22:59:07 +0200

    Update

--- istoria/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-@app.route('/')
-def index():
-    with open("index.html", "r") as f: return f.read()
-app.run(host="0.0.0.0", port=80, debug=True)
